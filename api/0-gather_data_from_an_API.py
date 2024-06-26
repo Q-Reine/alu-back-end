eb5db939c684212ec495e6348583502a5abb20cc
Q-Reine <q.wihogora@alustudent.com>
2024-06-26 22:22:40 +0200

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,28 +1,63 @@
 #!/usr/bin/python3
 """
-Using a REST API, and a given emp_ID, return info about their TODO list.
+a Python script that, using this REST API,
+https://jsonplaceholder.typicode.com/path/?query#fragments
+for a given employee ID, returns information about his/her
+TODO list progress.
+
+Requirements:
+use urllib or requests module
+The script must accept an integer as a parameter,
+which is the employee ID
+The script must display on the standard output the
+employee TODO list progress in this exact format:
+
+First line: Employee EMPLOYEE_NAME is done with
+tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
+EMPLOYEE_NAME: name of the employee
+NUMBER_OF_DONE_TASKS: number of completed tasks
+TOTAL_NUMBER_OF_TASKS: total number of tasks, which is
+the sum of completed and non-completed tasks
+
+Second and N next lines display the title of completed tasks:
+TASK_TITLE (with 1 tabulation and 1 space before the TASK_TITLE)
 """
-import requests
-import sys
-
-
 if __name__ == "__main__":
-    """ main section """
-    BASE_URL = 'https://jsonplaceholder.typicode.com'
-    employee = requests.get(
-        BASE_URL + f'/users/{sys.argv[1]}/').json()
-    EMPLOYEE_NAME = employee.get("name")
-    employee_todos = requests.get(
-        BASE_URL + f'/users/{sys.argv[1]}/todos').json()
-    serialized_todos = {}
+    import json
+    import sys
+    import urllib.request
 
-    for todo in employee_todos:
-        serialized_todos.update({todo.get("title"): todo.get("completed")})
-
-    COMPLETED_LEN = len([k for k, v in serialized_todos.items() if v is True])
-    print("Employee {} is done with tasks({}/{}):".format(
-        EMPLOYEE_NAME, COMPLETED_LEN, len(serialized_todos)))
-    for key, val in serialized_todos.items():
-        if val is True:
-            print("\t {}".format(key))
-
+    """
+    format the employees id with the url
+    https://jsonplaceholder.typicode.com/users/{employees_id}
+    after getting it from the command line using the sys module
+    """
+    employee_id = sys.argv[1]
+    url1 = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    url2 = f"https://jsonplaceholder.typicode.com/users/{employee_id}/"
+    # create a request objects at first using urllib.request.Request()
+    req_object1 = urllib.request.Request(url1, method="GET")
+    req_object2 = urllib.request.Request(url2, method="GET")
+    # fetch the resources using the request objects and the function
+    # urllib.request.urlopen
+    with urllib.request.urlopen(req_object1) as response_object1:
+        response1 = json.load(response_object1)
+    with urllib.request.urlopen(req_object2) as response_object2:
+        response2 = json.load(response_object2)
+    # create an empty list to store completed tasks
+    completed_tasks = []
+    # iterate through all task to get task with boolean value true
+    for task in response1:
+        if task['completed'] is not True:
+            continue
+        completed_tasks.append(task)
+    # get length for completed task and all task (complete and incomplete)
+    no_of_comptasks = len(completed_tasks)
+    totalno_of_task = len(response1)
+    # get the employee name
+    employee_name = response2["name"]
+    # display the required format in the docs
+    print(f"Employee {employee_name} is done with tasks({no_of_comptasks}/\
+{totalno_of_task}):")
+    for comp_tasks in completed_tasks:
+        print(f"\t {comp_tasks['title']}")
